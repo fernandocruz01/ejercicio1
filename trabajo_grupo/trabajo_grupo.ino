c11b3662ded1b4128f2a563c9678a2fbd4b93415
david <david.cruz01@correo.usa.edu.co>
2022-03-31 13:08:36 -0500

    tercer intento de arreglo

--- trabajo_grupo/trabajo_grupo.ino
+++ trabajo_grupo/trabajo_grupo.ino
@@ -2,6 +2,7 @@ const int ledPIN = 8;
 const int sensorherradura = 7;
 int vector[50];
 int i = 0;
+int infrarojo = 0;
  
 void setup() {
   //declarar variables y puerto serial
@@ -14,13 +15,12 @@ void loop(){
  //llenar base de datos
   vector[i] = digitalRead(sensorherradura);
  //sensado
-  digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
-  delay(1000);                   // esperar un segundo
-  digitalWrite(ledPIN , LOW);    // poner el Pin en LOW
-  delay(1000);
+  infrarojo = digitalRead(sensorherradura);   // leer el sensor
+  delay(1000); 
   
   // actuador
 
 
   //consola
+  i++;
 }
